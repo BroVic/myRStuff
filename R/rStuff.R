584e9224d36d247fdd36fba479c837884d2543b2
Victor Ordu <victorordu@outlook.com>
2017-05-15 18:33:48 +0100

    Improves the function's documentation

--- R/rStuff.R
+++ R/rStuff.R
@@ -1,9 +1,24 @@
-# Practice on the base package
-checkBasePkg <- function() { # TODO: Upgrade to a generic version
-  wd <- "C:/Users/Admn/Documents/5-Personal/Study/R/practice/base"
-  help(package = "base")
-  message("Documentation for package 'base' is now open")
+#' Prepare a practice environment
+#' 
+#' Reviews a package by opening its documentation, navigating to the relevant
+#' working directory (if necessary)
+#' 
+#' @param package name of the package
+#' 
+#' @return There is no return value. The side effects of the function call
+#' a change of the working directory (where applicable) and the opening of
+#' the documentation for that package.
+#' @export
+review.package <- function(package = character()) {
+  wd <- paste0("C:/Users/Admn/Documents/5-Personal/Study/R/practice/",
+               as.character(package))
+  
+  help(package = as.character(package))
+  message(paste("Documentation for the", sQuote(package),
+                "package is now open."))
+  
   if (getwd() != wd) {
-    message(paste("Working directory changed to", getwd()))
-  } else message(paste("Already in", wd))
+    setwd(wd)
+    message(paste0("Working directory changed to ", sQuote(getwd()), "."))     
+  } else message(paste0("Already in ", sQuote(wd), "."))
 }
