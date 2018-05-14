a12bb88cdb9b74516a06397c063e593e1c464294
Victor Ordu <victorordu@outlook.com>
2018-05-14 16:30:50 +0100

    Added some checks to custom R installation
    We now check whether there is even a need to carry out the update. Also we ensure that we are able to download the installer to an appropriate folder and where this does not work out, we stop the function from executing.

--- R/package-functions.R
+++ R/package-functions.R
@@ -43,9 +43,8 @@ which_pkg_missing_description <- function(lib)
   pkgs <- list.dirs(lib, recursive = FALSE)
   if (!length(pkgs))
     stop(sprintf("There are no packages in '%s'.", lib))
-  desc <- "DESCRIPTION"
   hasDESC <- sapply(pkgs, function(P) {
-    file.exists(file.path(lib, P, desc))
+    file.exists(file.path(lib, P, "DESCRIPTION"))
   })
   pkgs[!which(hasDESC)]
 }
@@ -59,31 +58,52 @@ which_pkg_missing_description <- function(lib)
 #' Installing R using customised settings of
 #' \code{\link[installr]{updateR}}
 #' 
+#' @details The R installation will be carried out without a recourse to 
+#' any kind of interaction, with the exception of a possible prompt from
+#' \emph{Windows Smartscreen}.
+#' 
 #' @importFrom installr updateR
+#' @importFrom installr check.for.updates.R
 #' 
 #' @export
 install_R_my_way <- function() {
+  
+  ## Determine download location
   if (identical(.Platform$OS.type, 'windows')) {
-    downs <- file.path(path.expand("~"), "Downloads")
-    if (!dir.exists(downs))
-      downs <- "C:/"
-    message("Download directory for the installer was set to ", downs)
+    dwn <- file.path(path.expand("~"), "Downloads")
+    if (!dir.exists(dwn)) {
+      cat("Creating the new directory", dwn, "... ")
+      if (dir.create(dwn)) {
+        cat("Successful\n")
+      }
+      else {
+        cat("Failed\n")
+        stop("You may need to check your permissions.")
+      }
+    }
+    message("The installer was downloaded to ", dwns)
+    stop("Attempted to run on an unsupported platform")
   }
-
-  updateR(
-    browse_news = FALSE,
-    install_R = TRUE,
-    copy_packages = TRUE,
-    copy_Rprofile.site = TRUE,
-    keep_old_packages = TRUE,
-    GUI = TRUE,
-    to_checkMD5sums = TRUE,
-    update_packages = FALSE,
-    start_new_R = FALSE,
-    quit_R = FALSE,
-    print_R_versions = FALSE,
-    keep_install_file = TRUE,
-    download_dir = downs,
-    silent = TRUE
-  )
+  
+  if (check.for.updates.R(notify_user = F)) {
+    updateR(
+      browse_news = FALSE,
+      install_R = TRUE,
+      copy_packages = TRUE,
+      copy_Rprofile.site = TRUE,
+      keep_old_packages = TRUE,
+      GUI = TRUE,
+      to_checkMD5sums = TRUE,
+      update_packages = FALSE,
+      start_new_R = FALSE,
+      quit_R = FALSE,
+      print_R_versions = FALSE,
+      keep_install_file = TRUE,
+      download_dir = dwn,
+      silent = TRUE
+    )
+  }
+  else {
+    cat("Nothing to be done.")
+  }
 }
