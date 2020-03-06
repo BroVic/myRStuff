cf74d7572b3463b7a5c5e7a89f68c07b112f50fc
Victor Ordu <victorordu@outlook.com>
2020-03-06 16:13:19 +0100

    Add case to installation
    In case the latest version has already been downloaded, we don't want to repeat that process.

--- R/install-r.R
+++ R/install-r.R
@@ -7,6 +7,7 @@
 #' 
 #' @importFrom installr check.for.updates.R
 #' @importFrom installr updateR
+#' @importFrom stringi stri_extract
 #' 
 #' @details The R installation will be carried out with little or no
 #' interaction from the user, with the exception of a possible prompt from
@@ -26,12 +27,31 @@ easy_updateR <- function() {
       keep_install_file <- FALSE
     }
   }
+  else {
+    myVer <-
+      as.numeric_version(paste0(R.version$major, ".", R.version$minor))
+    r.ver.pat <- "R-[d](\\.[d]){2}"
+    r.install.pat <- paste0(r.ver.pat, "-win\\.exe$")
+    rexecs <- list.files(dwn, pattern = r.install.pat)
+    if (!identical(rexecs, character(0))) {
+      rvers <- stringi::stri_extract(rexecs, regex = r.ver.pat)
+      rvers <- sort(as.numeric_version(rvers), decreasing = TRUE)
+    }
+  }
   
   updated <- FALSE
-  try({
-    if (!check.for.updates.R(notify_user = FALSE, GUI = FALSE)) 
-      cat("Nothing to do\n")
-    else updated <-
+  
+  if (myVer < rvers[1]) {
+    shell.exec(file.path(dwn, list.files(dwn, pattern = paste0(
+      rvers[1], "-win\\.exe$"
+    ))))
+    updated <- TRUE
+  }
+  else if (!check.for.updates.R(notify_user = FALSE, GUI = FALSE))
+    cat("Nothing to do\n")
+  else
+    try({
+      updated <-
         installr::updateR(
           browse_news = FALSE,
           install_R = TRUE,
@@ -48,7 +68,8 @@ easy_updateR <- function() {
           download_dir = dwn,
           silent = TRUE
         )
-  })
+    }
+    )
   if (updated && keep_install_file)
     message("The installer was saved to ", dwn)
   
