5a094d2800aa3f86c9db1390a072279f9177ae47
Victor Ordu <victorordu@outlook.com>
2020-11-13 14:24:41 +0100

    Fix failure in changing theme
    There as a mispelled theme name, as well as syntax error in the function.
    
    Bumped the patch version.

--- R/custom.R
+++ R/custom.R
@@ -10,8 +10,9 @@ toggleTheme <- function()
 {
   if (!isAvailable())
     stop("Not an RStudio session")
-  themes <- system.file('yaml', 'themes.yml', package = 'myRstuff')
+  themefile <- system.file('yaml', 'themes.yml', package = 'myRstuff')
   cur <- getThemeInfo()$editor
+  themes <- unlist(read_yaml(themefile))
   ind <- ifelse(cur == themes[1], 2L, 1L)
   applyTheme(themes[ind])
 }
