a76cdbe65e8c0a978e911c37f526954acfa7de08
Victor Ordu <victorordu@outlook.com>
2024-09-13 22:36:30 +0100

    Simplify process for updating R
    Much of the complexity has been removed from the R update function. The effort to control timeout via an argument has been abandoned. Any control of the timeout will be done outside this function.

--- R/install-r.R
+++ R/install-r.R
@@ -4,94 +4,76 @@
 #'
 #' A customised update of an existing R installation, without a fuss. This
 #' function is a wrapper for \code{\link[installr]{updateR}}
-#' 
-#' @param timeout integer; timeout for the download.
-#' 
+#'  
 #' @importFrom installr check.for.updates.R
 #' @importFrom installr updateR
-#' @importFrom stringi stri_extract
 #' 
 #' @details The R installation will be carried out with little or no
 #' interaction from the user, with the exception of a possible prompt from
-#' \emph{Windows Smartscreen}. This functionality is for Windows machines
+#' \emph{Windows Smartscreen}. This functionality is for Windows machines only
 #' - the function carries out a platform check internally.
+#' 
+#' @note
+#' In instances where only part of the installer is downloaded due to a timeout,
+#' one should consider resetting the default timeout (see example)
+#' 
+#' @examples
+#' \dontrun{
+#' # To extend the timeout period
+#' op <- options(timeout = 1200)  # timeout is reset to 20 mins
+#' easy_updateR()
+#' options(op)  # reset to original value
+#' }
+#' 
 #'
 #' @export
-easy_updateR <- function(timeout = getOption("timeout")) {
-  # TODO: CRAN reviewers usually frown upon modifying options
-  # within a function's implementation, so I will likely 
-  # revisit this a little later.
-  if (!is.numeric(timeout))
-    stop("'timeout' must be numeric")
+easy_updateR <- function() {
+  if (!identical(.Platform$OS.type, "windows"))
+    stop("This function runs only on Windows OS")
   
-  op <- options(timeout = as.integer(timeout))
-  on.exit(options(op))
+  # It's possible that the installer already exists on the
+  # system - this makes repeated downloads unnecessary. We
+  # would want to ascertain this. We are going to assume that
+  # the user has the default `Downloads` folder on their system.
+  dwndir <- file.path(Sys.getenv("HOME"), "Downloads")
   
-  if (!identical(.Platform$OS.type, tolower(win32 <- "Windows")))
-    stop("Function runs only on ", win32)
-  
-  dwn <- path.expand("~/Downloads")
-  keep_install_file <- TRUE
-  
-  if (!dir.exists(dwn)) {
-    if (!dir.create(dwn)) {
-      dwn <- getwd()
-      keep_install_file <- FALSE
-    }
-  }
-  
-  r.ver.pattern <- "R-[d](\\.[d]){2}"
-  r.installers <- 
-    list.files(dwn, pattern = paste0(r.ver.pattern, "-win\\.exe$"))
-  
-  if (length(r.installers) != 0L) {
-    r.versions <- stringi::stri_extract(r.installers, regex = r.ver.pattern)
-    mostRecent <- sort(as.numeric_version(r.versions), decreasing = TRUE)[1]
-    myVer <-
-      as.numeric_version(paste0(R.version$major, ".", R.version$minor))
+  if (dir.exists(dwndir)) {
+    rver.pat <- "R-[0-9](\\.[0-9]){2}-win\\.exe$"
+    r.installers <- list.files(dwndir, rver.pat)
     
-    if (myVer < mostRecent) {
-      shell.exec(file.path(dwn,
-                           list.files(dwn, pattern = paste0(
-                             mostRecent, "-win\\.exe$"
-                           ))))
-      return(TRUE)
+    if (length(r.installers) > 0L) {
+      r.versions <- stringr::str_extract(r.installers, pattern = rver.pat)
+      mostRecent <- sort(as.numeric_version(r.versions), decreasing = TRUE)[1]
+      myVer <- as.numeric_version(paste0(R.version$major, ".", R.version$minor))
+      
+      if (myVer < mostRecent) {
+        shell.exec(file.path(dwndir, list.files(
+          dwndir, pattern = paste0(mostRecent, "-win\\.exe$")
+        )))
+        
+        return(TRUE)
+      }
     }
   }
-  
-  if (!check.for.updates.R(notify_user = FALSE, GUI = FALSE)) {
-    cat("Nothing to do\n")
+  else
+    dwndir <- tempdir()
+
+  if (!check.for.updates.R(GUI = FALSE))
     return(FALSE)
-  }
   
-  tryCatch({
-    updated <-
-      installr::updateR(
-        browse_news = FALSE,
-        install_R = TRUE,
-        copy_packages = FALSE,
-        copy_Rprofile.site = TRUE,
-        keep_old_packages = TRUE,
-        GUI = TRUE,
-        to_checkMD5sums = TRUE,
-        update_packages = FALSE,
-        start_new_R = FALSE,
-        quit_R = FALSE,
-        print_R_versions = FALSE,
-        keep_install_file = keep_install_file,
-        download_dir = dwn,
-        silent = TRUE
-      )
-    
-    if (!updated)
-      stop("R could not be updated")
-    
-  }, error = function(e) {
-    stop(e)
-  })
-  
-  if (updated && keep_install_file)
-    message("The installer was saved to ", sQuote(dwn))
-  
-  updated
+  updateR(
+    browse_news = FALSE,
+    install_R = TRUE,
+    copy_packages = FALSE,
+    copy_site_files = TRUE,
+    keep_old_packages = TRUE,
+    update_packages = FALSE,
+    start_new_R = FALSE,
+    quit_R = FALSE,
+    print_R_versions = FALSE,
+    to_checkMD5sums = TRUE,
+    keep_install_file = TRUE,
+    download_dir = dwndir,
+    silent = TRUE
+  )
 }
